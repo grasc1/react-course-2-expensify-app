75b86b338d3a01392b2c6b251500ad1c40d1525e
grasc1 <christophe.grasso@gmail.com>
2020-09-15 21:08:28 +0200

    Setup test database env

---
+++ src/firebase/firebase.js
@@ -0,0 +1,158 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+firebase.initializeApp(config);
+// firebase.analytics();
+
+const database = firebase.database();
+
+export { firebase, database as default };
+
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// const onValueChange = database.ref('expenses').on('value', (snapshot) => {
+//     const expenses = [];
+//     snapshot.forEach((childSnapshot) => {
+//         expenses.push({
+//             id: childSnapshot.key,
+//             ...childSnapshot.val()
+//         });
+//     });
+//     console.log(expenses);
+// }, (e) => {
+//     console.log('Error with data fetching', e);
+// });
+
+// database.ref('expenses')
+//     .once('value')
+//     .then((snapshot) => {
+//         const expenses = [];
+//         snapshot.forEach((childSnapshot) => {
+//             expenses.push({
+//                 id: childSnapshot.key,
+//                 ...childSnapshot.val()
+//             });
+//         });
+//         console.log(expenses);
+//     });
+
+// database.ref('expenses').push({
+//     description: 'Rent',
+//     note: '',
+//     amount: 109500,
+//     createdAt: 464654646463
+// });
+
+// database.ref('notes/-MH7i8sCCvXegIXzBaZT').remove();
+
+// database.ref('notes').push({
+//     title: 'Course Topics',
+//     body: 'React Native, Angular, Python'
+// });
+
+// const firebaseNotes = {
+//     notes: {
+//         asdfasdf: {
+//             title: 'First note',
+//             body: 'This is my note'
+//         },
+//         asdfaghsdfg: {
+//             title: 'Another note!',
+//             body: 'This is my note'
+//         }
+//     }
+// };
+
+// const notes = [{
+//     id: '12',
+//     title: 'first note!',
+//     body: 'This is my note'
+// }, {
+//     id: '761ase',
+//     title: 'Another note!',
+//     body: 'This is my note'
+// }];
+
+// database.ref('notes').set(notes);
+// database.ref('notes/12')
+
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//     const val = snapshot.val();
+//     console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+// }, (e) => {
+//     console.log('Error with data fetching', e);
+// });
+
+// setTimeout(() => {
+//     database.ref('age').set(29);
+// }, 3500);
+
+// setTimeout(() => {
+//     database.ref().off('value', onValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//     database.ref('age').set(30);
+// }, 10500);
+
+// database.ref()
+//     .once('value')
+//     .then((snapshot) => {
+//         const val = snapshot.val();
+//         console.log(val);
+//     })
+//     .catch((e) => {
+//         console.log('Error fetching data', e)
+//     });
+
+// database.ref().set({
+//     name: 'Christophe Grasso',
+//     age: 36,
+//     stressLevel: 6,
+//     job: {
+//         title: 'Software developer',
+//         company: 'Google'
+//     },
+//     location: {
+//         city: 'Biel/Bienne',
+//         country: "Switzerland"
+//     }
+// }).then(() => {
+//     console.log('Data is saved');
+// }).catch((e) => {;
+//     console.log('This failed. ', e);
+// });
+
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Amazon',
+//     'location/city': 'Seattle'
+// });
+
+// database.ref()
+//     .remove()
+//     .then(() => {
+//         console.log('Data was removed');
+//     }).catch((e) => {
+//         console.log('Did not remove data', e);
+//     });
